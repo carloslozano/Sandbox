ae6e827e0a586b88ff9b946971aecf9b13314fc8
Carlos Lozano <carlos@adaptive.me>
2015-12-07 22:15:42 +0000

    I hope I am done

---
+++ swift_rockstar.py
@@ -0,0 +1,6 @@
+from rockstar import RockStar
+
+swift_code = "print('Hello world')"
+rock_it_bro = RockStar(days=400, file_name='jvm2swift.swift', code=swift_code)
+rock_it_bro.make_me_a_rockstar()
+
